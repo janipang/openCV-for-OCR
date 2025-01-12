53ac4a5c2b9331bdd1d8fd3bd25a09d19ec92fe4
janipang <pang.janista@gmail.com>
2025-01-12 15:03:42 +0700

    chore: restructure docs-head-reader main file

--- docs-head-reader/app.py
+++ docs-head-reader/app.py
@@ -1,7 +1,7 @@
 import os
 import fitz
 import cv2
-from lib import scan_files, convert_file_to_image, check_document_type_by_name
+from lib import *
 
 SUPPORTED_DOCUMENT_TYPE = ["inv", "ca"]
 TEXT_COLOR_FADE_OFFSET =  120
@@ -15,72 +15,6 @@ INV_HEADER_POSITION = ((1548, 222), (2400, 1052))
 CA_HEADER_POSITION = ((1340, 353), (2380, 748)) 
 # CA_LEFT_SIDE = ((1791, 353), (1856, 748)) fake
 # CA_RIGHT_SIDE = ((1884, 353), (2300, 748)) fake
-
-def get_filename_only(filename):
-  return filename.split("_")[0]
-
-def isAllZero(dot_data):
-  for dot in dot_data:
-    if dot > DOT_PER_LINE_OFFSET:
-      return False
-  return True
-  
-def lastNisZero(dot_data, n = LINE_HEIGHT_OFFSET):
-  return isAllZero(dot_data[-n:])
-
-def save_line_image(image, y_start, y_stop, line_number, filename, document_type):
-  target_dir = f"./src/temp/{document_type}/lines/{filename}/header"
-  os.makedirs(target_dir, exist_ok=True)
-  cv2.imwrite(os.path.join(f"{target_dir}/line-{line_number + 1}.png"), image[y_start: y_stop + 1])
-  
-def get_header_image(image, mode):
-  match mode:
-    case "inv":
-      ((x_start, y_start), (x_end, y_end)) = INV_HEADER_POSITION
-    case "ca":
-      ((x_start, y_start), (x_end, y_end)) = CA_HEADER_POSITION
-  return image[y_start:y_end, x_start:x_end]
-  
-def split_line(header_image, filename, document_type):
-    # variables
-    line_start = 0
-    line_stop = 0
-    dot_data = []
-    
-    line_finished = False
-    lines_saved = 0
-    
-    # process line up to down
-    for line_num in range(len(header_image)):
-      line = header_image[line_num]
-      dot = 0
-      # process pixel left to reight
-      for pixel_num in range(len(line)):
-        pixel = line[pixel_num]
-        
-        # count dots up when pixel color is black
-        if sum(pixel.astype(int))/3 <= 0 + TEXT_COLOR_FADE_OFFSET:
-          dot += 1
-          
-      line_stop += 1
-      dot_data.append(dot)
-      
-      # if no dot appears move pointer down
-      if dot < DOT_PER_LINE_OFFSET and isAllZero(dot_data) and (line_stop - line_start) + 1 > LINE_HEIGHT_OFFSET: # line empty and stack full of empty lines
-        line_start += 1
-        dot_data.pop(0)
-        
-      # if reach the bottom of text
-      if not isAllZero(dot_data) and lastNisZero(dot_data):
-        line_finished = True
-      
-      # save picture when the line passed
-      if line_finished:
-        save_line_image(header_image, line_start, line_stop, lines_saved, filename, document_type)
-        lines_saved += 1
-        line_start = line_stop
-        dot_data = []
-        line_finished = False
 
 def main():
   
--- docs-head-reader/lib.py
+++ docs-head-reader/lib.py
@@ -2,6 +2,8 @@ import os
 import fitz
 import cv2
 
+from app import TEXT_COLOR_FADE_OFFSET, DOT_PER_LINE_OFFSET, LINE_HEIGHT_OFFSET, INV_HEADER_POSITION, CA_HEADER_POSITION
+
 def scan_files(folder_path):
     return os.listdir(folder_path)
 
@@ -33,3 +35,70 @@ def convert_file_to_image(
             pixmap = page.get_pixmap(dpi=300)
             pixmap.save(f"./src/temp/{document_type}/raw-file-png/{target_file_name}")
         file.close()
+
+
+def get_filename_only(filename):
+  return filename.split("_")[0]
+
+def isAllZero(dot_data):
+  for dot in dot_data:
+    if dot > DOT_PER_LINE_OFFSET:
+      return False
+  return True
+  
+def lastNisZero(dot_data, n = LINE_HEIGHT_OFFSET):
+  return isAllZero(dot_data[-n:])
+
+def save_line_image(image, y_start, y_stop, line_number, filename, document_type):
+  target_dir = f"./src/temp/{document_type}/lines/{filename}/header"
+  os.makedirs(target_dir, exist_ok=True)
+  cv2.imwrite(os.path.join(f"{target_dir}/line-{line_number + 1}.png"), image[y_start: y_stop + 1])
+  
+def get_header_image(image, mode):
+  match mode:
+    case "inv":
+      ((x_start, y_start), (x_end, y_end)) = INV_HEADER_POSITION
+    case "ca":
+      ((x_start, y_start), (x_end, y_end)) = CA_HEADER_POSITION
+  return image[y_start:y_end, x_start:x_end]
+  
+def split_line(header_image, filename, document_type):
+    # variables
+    line_start = 0
+    line_stop = 0
+    dot_data = []
+    
+    line_finished = False
+    lines_saved = 0
+    
+    # process line up to down
+    for line_num in range(len(header_image)):
+      line = header_image[line_num]
+      dot = 0
+      # process pixel left to reight
+      for pixel_num in range(len(line)):
+        pixel = line[pixel_num]
+        
+        # count dots up when pixel color is black
+        if sum(pixel.astype(int))/3 <= 0 + TEXT_COLOR_FADE_OFFSET:
+          dot += 1
+          
+      line_stop += 1
+      dot_data.append(dot)
+      
+      # if no dot appears move pointer down
+      if dot < DOT_PER_LINE_OFFSET and isAllZero(dot_data) and (line_stop - line_start) + 1 > LINE_HEIGHT_OFFSET: # line empty and stack full of empty lines
+        line_start += 1
+        dot_data.pop(0)
+        
+      # if reach the bottom of text
+      if not isAllZero(dot_data) and lastNisZero(dot_data):
+        line_finished = True
+      
+      # save picture when the line passed
+      if line_finished:
+        save_line_image(header_image, line_start, line_stop, lines_saved, filename, document_type)
+        lines_saved += 1
+        line_start = line_stop
+        dot_data = []
+        line_finished = False
