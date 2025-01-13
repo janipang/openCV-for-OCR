927df1714e87519b5bb48284a41ff901954725cd
janipang <pang.janista@gmail.com>
2025-01-14 03:19:20 +0700

    feat: record data to file success with confirmed space_tresh

--- docs-data-collector/data_collector.py
+++ docs-data-collector/data_collector.py
@@ -6,7 +6,7 @@ import numpy as np
 from PIL import ImageEnhance, ImageFilter, Image
 
 def get_key_value_position(image):
-    gap_threshold = 40
+    gap_threshold = 100
 
     # position format: [ y_start, y_end ]
     position = []
@@ -39,39 +39,42 @@ def get_key_value_position(image):
     if len(position) == 0:
         return "No phrase found."
     elif len(position) == 1:
-        return position[0]
+        return [position[0]]
     else:
-        return position[0], (position[1][0], position[-1][1])
+        return [position[0], [position[1][0], position[-1][1]]]
     
 def read_text_from_image(int_array_2d):
     uint8_array = np.array(int_array_2d).astype(np.uint8)
     custom_oem_psm_config = r'-l tha+end --psm 6'
     p_image = Image.fromarray(uint8_array)
-    print("Pillow image created successfully.")
     return pytesseract.image_to_string(p_image, config=custom_oem_psm_config)
 
 def save_data_from_lines(input_dir, output_file):
     # get data
     data = {}
     files_name = os.listdir(input_dir)
+    key = ""
+    value = ""
+    
     for file_name in files_name:
         img = cv2.imread(f"{input_dir}/{file_name}")
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         _, image = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY_INV)
 
-        key = ""
-        value = ""
+        
         position = get_key_value_position(image)
+        print(position)
         if len(position) == 2:
-            cv2.imwrite("./src/temp/reading/key.png", image[:, position[0][0]:position[0][1]])
-            cv2.imwrite("./src/temp/reading/value.png", image[:, position[1][0]:position[1][1]])
+            cv2.imwrite(f"./src/temp/reading/{file_name.split(".")[0]}-key.png", image[:, position[0][0]:position[0][1]])
+            cv2.imwrite(f"./src/temp/reading/{file_name.split(".")[0]}-value.png", image[:, position[1][0]:position[1][1]])
             key = read_text_from_image(image[:, position[0][0]:position[0][1]])
             value = read_text_from_image(image[:, position[1][0]:position[1][1]])
             data[key] = value
         elif len(position) == 1:
-            value = read_text_from_image(image[position[0][0]:position[0][1]])
+            cv2.imwrite(f"./src/temp/reading/{file_name.split(".")[0]}-value.png", image[:, position[0][0]:position[0][1]])
+            value = read_text_from_image(image[:, position[0][0]:position[0][1]])
             data[key] = data[key] + value
-        print(data)
+    print(data)
 
     # write to json
     json_object = json.dumps(data, indent=2)
