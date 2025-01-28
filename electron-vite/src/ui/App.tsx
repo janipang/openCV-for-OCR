39179eb6ae9060ea0062ce8b8900f2492491076e
janipang <pang.janista@gmail.com>
2025-01-28 17:59:02 +0700

    feat: setup vite successfully but not tsx in electron

--- electron-vite/src/ui/App.tsx
+++ electron-vite/src/ui/App.tsx
@@ -1,30 +1,90 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <aside>
+      <a href="/">
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M80-720v-200h200v80H160v120H80Zm720 0v-120H680v-80h200v200h-80ZM80-40v-200h80v120h120v80H80Zm600 0v-80h120v-120h80v200H680ZM280-240h400v-480H280v480Zm0 80q-33 0-56.5-23.5T200-240v-480q0-33 23.5-56.5T280-800h400q33 0 56.5 23.5T760-720v480q0 33-23.5 56.5T680-160H280Zm80-400h240v-80H360v80Zm0 120h240v-80H360v80Zm0 120h240v-80H360v80Zm-80 80v-480 480Z"/></svg>
+        <p>My Workspace</p>
+      </a>
+      <a href="/shared">
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v226q-19-9-39-14.5t-41-8.5v-203H200v360h168q9 27 30 47t47 28q-3 20-4 40.5t2 40.5q-36-7-67.5-26.5T320-320H200v120h253q7 22 16 42t22 38H200Zm0-80h253-253Zm80-410h400v-80H280v80Zm0 140h237q27-29 60.5-49t72.5-31H280v80ZM800-80q-33 0-56.5-23.5T720-161l-81-49q-9 5-18.5 7.5T600-200q-33 0-56.5-23.5T520-280q0-33 23.5-56.5T600-360q11 0 20.5 2.5T639-350l81-48q0-34 23.5-57.5T800-479q33 0 56.5 23.5T880-399q0 33-23.5 56.5T800-319q-11 0-20.5-2.5T761-329l-81 48v3l81 48q9-5 18.5-7.5T800-240q33 0 56.5 23.5T880-160q0 33-23.5 56.5T800-80Z"/></svg>
+        <p>Shared Files</p>
+      </a>
+      <a href="/contact">
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M40-160v-112q0-34 17.5-62.5T104-378q62-31 126-46.5T360-440q66 0 130 15.5T616-378q29 15 46.5 43.5T680-272v112H40Zm720 0v-120q0-44-24.5-84.5T666-434q51 6 96 20.5t84 35.5q36 20 55 44.5t19 53.5v120H760ZM360-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47Zm400-160q0 66-47 113t-113 47q-11 0-28-2.5t-28-5.5q27-32 41.5-71t14.5-81q0-42-14.5-81T544-792q14-5 28-6.5t28-1.5q66 0 113 47t47 113ZM120-240h480v-32q0-11-5.5-20T580-306q-54-27-109-40.5T360-360q-56 0-111 13.5T140-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T440-640q0-33-23.5-56.5T360-720q-33 0-56.5 23.5T280-640q0 33 23.5 56.5T360-560Zm0 320Zm0-400Z"/></svg>
+        <p>My Contact</p>
+      </a>
+      <a href="/profile">
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M234-276q51-39 114-61.5T480-360q69 0 132 22.5T726-276q35-41 54.5-93T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 59 19.5 111t54.5 93Zm246-164q-59 0-99.5-40.5T340-580q0-59 40.5-99.5T480-720q59 0 99.5 40.5T620-580q0 59-40.5 99.5T480-440Zm0 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q53 0 100-15.5t86-44.5q-39-29-86-44.5T480-280q-53 0-100 15.5T294-220q39 29 86 44.5T480-160Zm0-360q26 0 43-17t17-43q0-26-17-43t-43-17q-26 0-43 17t-17 43q0 26 17 43t43 17Zm0-60Zm0 360Z"/></svg>
+        <p>Profile</p>
+      </a>
+    </aside>
+    <main>
+      <section className="search">Automated Invoice Data Gathering using OCR</section>
+      <section className="raw-files section-content">
+        <h2>Raw Files</h2>
+        <span>
+          <div className="card">hello sir</div>
+          <div className="card">hello sir</div>
+          <div className="card">hello sir</div>
+          <div className="card">hello sir</div>
+          <div className="card">hello sir</div>
+        </span>
+      </section>
+      <section className="templates section-content">
+        <h2>Templates</h2>
+        <span>
+            <div className="card">hello sir</div>
+            <div className="card">hello sir</div>
+            <div className="card">hello sir</div>
+            <div className="card">hello sir</div>
+            <div className="card">hello sir</div>
+          </span>
+      </section>
+      <section className="recent-files section-content">
+        <h2>Recent Files</h2>
+        <span className="list">
+          <div className="list-item">hello sir</div>
+          <div className="list-item">hello sir</div>
+          <div className="list-item">hello sir</div>
+          <div className="list-item">hello sir</div>
+          <div className="list-item">hello sir</div>
+          <div className="list-item">hello sir</div>
+          <div className="list-item">hello sir</div>
+          <div className="list-item">hello sir</div>
+          </span>
+      </section>
+      <article className="process">
+        <div className="file-dropzone">select files here</div>
+        <section className="select section-content">
+          <h3>Select Template</h3>
+          <div className="template-select">
+            <span className="card">
+              <div><span>Template1</span><span></span></div>
+            </span>
+            <span className="card">
+              <div><span>Template2</span><span></span></div>
+            </span>
+            <span className="card">
+              <div><span>Template3</span><span></span></div>
+            </span>
+          </div>
+        </section>
+        <section className="filename section-content">
+          <h3>Output File Name</h3>
+          <span className="input-name">
+            <input type="text"/>
+            <p>.csv</p>
+          </span>
+        </section>
+        <button className="submit">Process</button>
+      </article>
+    </main>
     </>
   )
 }
