9f38590ec5ff295973ce7d0407a4cb5dcb1da4f1
janipang <pang.janista@gmail.com>
2025-02-02 01:42:32 +0700

    feat(electron): add processing files

--- electron-cra/preload.js
+++ electron-cra/preload.js
@@ -3,3 +3,7 @@ const { contextBridge, ipcRenderer } = require("electron");
 contextBridge.exposeInMainWorld("electron", {
     copyFiles: (files) => ipcRenderer.invoke("copy-files", files),
 });
+
+contextBridge.exposeInMainWorld("electron", {
+    processFiles: (processFolder) => ipcRenderer.invoke("process-files", processFolder),
+});
