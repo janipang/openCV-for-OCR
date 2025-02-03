b5fe8b27367db6e0b19f6829a93f235d4c09a532
janipang <pang.janista@gmail.com>
2025-02-03 22:55:49 +0700

    feat(electron): installing dependencies when run process

--- electron-cra/app/src/component/file-upload.tsx
+++ electron-cra/app/src/component/file-upload.tsx
@@ -1,9 +1,13 @@
 export default function FileUpload() {
     const selectedPaths = [
-        "D:/Code/Project/Software/openCV-for-OCR/src/project-docs-1/CA202411030001.pdf",
-        "D:/Code/Project/Software/openCV-for-OCR/src/project-docs-1/CA202412010002.pdf",
-        "D:/Code/Project/Software/openCV-for-OCR/src/project-docs-1/CA202412010004.pdf",
-        "D:/Code/Project/Software/openCV-for-OCR/src/project-docs-1/CA202412010005.pdf",
+        // "D:/Code/Project/Software/openCV-for-OCR/src/project-docs-1/CA202411030001.pdf",
+        // "D:/Code/Project/Software/openCV-for-OCR/src/project-docs-1/CA202412010002.pdf",
+        // "D:/Code/Project/Software/openCV-for-OCR/src/project-docs-1/CA202412010004.pdf",
+        // "D:/Code/Project/Software/openCV-for-OCR/src/project-docs-1/CA202412010005.pdf",
+        "D:/projects/openCV-for-OCR/src/project-docs-1/CA202411020001.pdf",
+        "D:/projects/openCV-for-OCR/src/project-docs-1/CA202411020002.pdf",
+        "D:/projects/openCV-for-OCR/src/project-docs-1/INV202411050003.pdf",
+        "D:/projects/openCV-for-OCR/src/project-docs-1/INV202411050004.pdf",
     ]
     async function onSelectFiles(paths: string[]) {
         try {
--- electron-cra/main.js
+++ electron-cra/main.js
@@ -1,113 +1,148 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
-const url = require('url');
-const path = require('path');
-const fs = require('fs');
-const { spawn } = require('child_process');
+const { app, BrowserWindow, ipcMain } = require("electron");
+const url = require("url");
+const path = require("path");
+const fs = require("fs");
+const { spawn, exec } = require("child_process");
 
-const baseTempDir = path.join(app.getPath("temp"), "invoice-data-gathering-app");
+const baseTempDir = path.join(
+  app.getPath("temp"),
+  "invoice-data-gathering-app"
+);
 
 // Define folders
 const folders = {
-    base: baseTempDir,
-    raw: path.join(baseTempDir, "src", "raw-file"),
-    output: path.join(baseTempDir, "src", "output"),
-    temp: path.join(baseTempDir, "src", "temp")
+  base: baseTempDir,
+  raw: path.join(baseTempDir, "src", "raw-file"),
+  output: path.join(baseTempDir, "src", "output"),
+  temp: path.join(baseTempDir, "src", "temp"),
 };
 
 function createWindow() {
-    let win = new BrowserWindow({
-        title: 'Electron + Create React App',
-        width: 800,
-        height: 600,
-        webPreferences: {
-            preload: path.join(__dirname, 'preload.js'),
-            enableRemoteModule: false,
-            contextIsolation: true,
-        }
-    });
+  let win = new BrowserWindow({
+    title: "Electron + Create React App",
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      enableRemoteModule: false,
+      contextIsolation: true,
+    },
+  });
 
-    const startUrl = url.format({
-        pathname: path.join(__dirname, 'app/dist/index.html'),
-        protocol: 'file:'
-    })
-    console.log(startUrl);
-    win.loadURL(startUrl);
+  const startUrl = url.format({
+    pathname: path.join(__dirname, "app/dist/index.html"),
+    protocol: "file:",
+  });
+  win.loadURL(startUrl);
 }
 
-
 app.whenReady().then(() => {
-    createWindow();
+  createWindow();
 
-    ipcMain.handle("copy-files", async (_event, filepaths) => {
+  ipcMain.handle("copy-files", async (_event, filepaths) => {
+    const rawFilesFolder = folders.raw;
+    console.log("Copying files from to ", rawFilesFolder, "...");
 
-        const inputFilesFolder = folders.raw
-        console.log(inputFilesFolder);
+    // create raw-file folder
+    if (fs.existsSync(rawFilesFolder)) {
+      fs.rmSync(rawFilesFolder, { recursive: true, force: true });
+    }
+    fs.mkdirSync(rawFilesFolder, { recursive: true });
 
-        if (fs.existsSync(inputFilesFolder)) {
-            fs.rmSync(inputFilesFolder, { recursive: true, force: true });
-        }
-        fs.mkdirSync(inputFilesFolder, { recursive: true });
-
-        filepaths.forEach((filepath) => {
-            const fileName = path.basename(filepath);
-            fs.copyFileSync(filepath, path.join(inputFilesFolder, fileName));
-        });
-
-        return true;
+    filepaths.forEach((filepath) => {
+      const fileName = path.basename(filepath);
+      fs.copyFileSync(filepath, path.join(rawFilesFolder, fileName));
     });
 
-    ipcMain.handle("process-files", async (_event) => {
-        console.log("Processing files");
+    return true;
+  });
 
-        const pythonFileNames = ["app.py", "data_collector.py", "header_scanner.py", "table_scanner.py"];
-        pythonFileNames.forEach((fileName) => {
-            const filePath = path.join(__dirname, "resources", fileName);
-            fs.copyFileSync(filePath, path.join(folders.base, fileName));
-        });
+  ipcMain.handle("process-files", async (_event) => {
 
-        const outputFolder = folders.output
-        if (fs.existsSync(outputFolder)) {
-            fs.rmSync(outputFolder, { recursive: true, force: true });
-        }
-        fs.mkdirSync(outputFolder, { recursive: true });
-        
-        const tempFolder = folders.temp
-        if (fs.existsSync(tempFolder)) {
-            fs.rmSync(tempFolder, { recursive: true, force: true });
-        }
-        fs.mkdirSync(tempFolder, { recursive: true });
-
-        return new Promise((resolve, reject) => {
-            const pythonScript = path.join(folders.base, "app.py");
-
-            const pythonProcess = spawn("python", [pythonScript, folders.base]);
-
-            pythonProcess.stdout.on("data", (data) => {
-                console.log(`Python Output: ${data}`);
-            });
-
-            pythonProcess.stderr.on("data", (data) => {
-                console.error(`Python Error: ${data}`);
-            });
-
-            pythonProcess.on("close", (code) => {
-                if (code === 0) {
-                    resolve("Processing completed");
-                } else {
-                    reject(new Error("Processing failed"));
-                }
-            });
-        });
+    // add resources file
+    console.log("Copying Resources files...");
+    const pythonFileNames = [
+      "app.py",
+      "data_collector.py",
+      "header_scanner.py",
+      "table_scanner.py",
+      "custom_service.py",
+      "requirements.txt",
+    ];
+    pythonFileNames.forEach((fileName) => {
+      const filePath = path.join(__dirname, "resources", fileName);
+      fs.copyFileSync(filePath, path.join(folders.base, fileName));
     });
+
+    // create output folder
+    const outputFolder = folders.output;
+    if (fs.existsSync(outputFolder)) {
+      fs.rmSync(outputFolder, { recursive: true, force: true });
+    }
+    fs.mkdirSync(outputFolder, { recursive: true });
+
+    // create temp folder
+    const tempFolder = folders.temp;
+    if (fs.existsSync(tempFolder)) {
+      fs.rmSync(tempFolder, { recursive: true, force: true });
+    }
+    fs.mkdirSync(tempFolder, { recursive: true });
+
+    // pip install dependencies
+    console.log("Installing Dependencies...");
+    const requirementsFile = path.join(folders.base, "requirements.txt");
+    const installDeps = spawn("pip", ["install", "-r", requirementsFile], {
+      shell: true,
+    });
+
+    installDeps.stdout.on("data", (data) => {
+      console.log(`pip install: ${data}`);
+    });
+
+    installDeps.stderr.on("data", (data) => {
+      console.error(`pip install error: ${data}`);
+    });
+
+    installDeps.on("close", (code) => {
+      if (code === 0) {
+        console.log("Dependencies installed. Running app.py...");
+
+        // run python app file
+        process_status = new Promise((resolve, reject) => {
+          const pythonScript = path.join(folders.base, "app.py");
+          const pythonProcess = spawn("python", [pythonScript, folders.base]);
+
+          pythonProcess.stdout.on("data", (data) => {
+            console.log(`Python Output: ${data}`);
+          });
+
+          pythonProcess.stderr.on("data", (data) => {
+            console.error(`Python Error: ${data}`);
+          });
+
+          pythonProcess.on("close", (code) => {
+            if (code === 0) {
+              resolve("Processing completed");
+            } else {
+              reject(new Error("Processing failed"));
+            }
+          });
+        });
+        return process_status;
+      } else {
+        console.error("Failed to install dependencies.");
+        return false;
+      }
+    });
+  });
 });
 
-
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        const appTempFolder = folders.base
-        if (fs.existsSync(appTempFolder)) {
-            fs.rmSync(appTempFolder, { recursive: true, force: true });
-        }
-        app.quit();
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    const appTempFolder = folders.base;
+    if (fs.existsSync(appTempFolder)) {
+      fs.rmSync(appTempFolder, { recursive: true, force: true });
     }
+    app.quit();
+  }
 });
