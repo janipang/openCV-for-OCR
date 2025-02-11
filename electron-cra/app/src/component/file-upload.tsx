e5801cf5034bf64957521152a1fd93c393598341
janipang <pang.janista@gmail.com>
2025-02-11 13:32:16 +0700

    feat(electron): re-route and file upload

--- electron-cra/app/src/component/file-upload.tsx
+++ electron-cra/app/src/component/file-upload.tsx
@@ -1,26 +1,43 @@
+import { useState } from 'react';
+import './file-upload.css';
+
 export default function FileUpload() {
-    const selectedPaths = [
-        // "D:/Code/Project/Software/openCV-for-OCR/src/project-docs-1/CA202411030001.pdf",
-        // "D:/Code/Project/Software/openCV-for-OCR/src/project-docs-1/CA202412010002.pdf",
-        // "D:/Code/Project/Software/openCV-for-OCR/src/project-docs-1/CA202412010004.pdf",
-        // "D:/Code/Project/Software/openCV-for-OCR/src/project-docs-1/CA202412010005.pdf",
-        "D:/projects/openCV-for-OCR/src/project-docs-1/CA202411020001.pdf",
-        "D:/projects/openCV-for-OCR/src/project-docs-1/CA202411020002.pdf",
-        "D:/projects/openCV-for-OCR/src/project-docs-1/INV202411050003.pdf",
-        "D:/projects/openCV-for-OCR/src/project-docs-1/INV202411050004.pdf",
-    ]
-    async function onSelectFiles(paths: string[]) {
-        try {
-            const result = await window.electron.copyFiles(paths);
-            console.log("Message sent, received in main process:", result);
-        } catch (error) {
-            console.error("Error in sending message:", error);
-        }
+    const [files, setFiles] = useState<any>([]);
+
+    const handleFileUpload = (newFiles: any[]) => {
+        setFiles([...newFiles]);
     }
+
+    console.log(files);
+
+    // const selectedPaths = [
+    //     "D:/Code/Project/Software/openCV-for-OCR/src/project-docs-1/CA202411030001.pdf",
+    //     "D:/Code/Project/Software/openCV-for-OCR/src/project-docs-1/CA202412010002.pdf",
+    //     "D:/Code/Project/Software/openCV-for-OCR/src/project-docs-1/CA202412010004.pdf",
+    //     "D:/Code/Project/Software/openCV-for-OCR/src/project-docs-1/CA202412010005.pdf",
+    //     // "D:/projects/openCV-for-OCR/src/project-docs-1/CA202411020001.pdf",
+    //     // "D:/projects/openCV-for-OCR/src/project-docs-1/CA202411020002.pdf",
+    //     // "D:/projects/openCV-for-OCR/src/project-docs-1/INV202411050003.pdf",
+    //     // "D:/projects/openCV-for-OCR/src/project-docs-1/INV202411050004.pdf",
+    // ]
+    // async function onSelectFiles(paths: string[]) {
+    //     try {
+    //         const result = await window.electron.copyFiles(paths);
+    //         console.log("Message sent, received in main process:", result);
+    //     } catch (error) {
+    //         console.error("Error in sending message:", error);
+    //     }
+    // }
 
     return (
         <div className="file-dropzone">
-            <button onClick={() => onSelectFiles(selectedPaths)}>select files here</button>
+            <input id="file-input" type="file" multiple onChange={(e) => {
+                const files = e.target.files;
+                if (files) {
+                    handleFileUpload(Array.from(files));
+                }
+            }} />
+            <label htmlFor="file-input">Select Files to Process</label>
         </div>
     )
 }
--- electron-cra/app/src/main.tsx
+++ electron-cra/app/src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/home';
+import RecentPage from './pages/recent';
+import Layout from './pages/layout';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="recent" element={<RecentPage />} />
+        </Route>
+      </Routes>
+    </HashRouter>
+  </StrictMode>
+);
