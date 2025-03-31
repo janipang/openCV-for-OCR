cb4f80aaa69f8b19349178b0b9f1996d0cfecf28
janipang <pang.janista@gmail.com>
2025-03-31 22:13:26 +0700

    feat: alignment/sorting backup item

--- electron-cra/app/src/pages/home.tsx
+++ electron-cra/app/src/pages/home.tsx
@@ -148,6 +148,7 @@ export default function HomePage() {
     setInputFiles([]);
     setOutputDir("");
     setOutputFileName("");
+    setResultFilePath("");
     setProcessStatus("waiting");
   }
 
